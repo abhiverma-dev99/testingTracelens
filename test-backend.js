7405d14cd2a6e5f23aa804422c12aa1894a5679e
abhiverma-dev99 <abhiverma.dev99@gmail.com>
2026-09-06 00:10:47 +0530

    testing from render

--- test-backend.js
+++ test-backend.js
@@ -4,17 +4,19 @@ async function triggerLiveError() {
   console.log("Starting production auth service...");
   
   try {
-    // FAKE BUG 2: API response null aayi, aur hum us par .map() chala rahe hain
-    const apiResponse = null;
+    // FAKE BUG 3: Token validation fail ho gaya aur object undefined reh gaya
+    const decodedToken = undefined;
     
-    // Yeh line crash hogi: Cannot read properties of null (reading 'map')
-    const userNames = apiResponse.map(user => user.name); 
+    // Yeh line crash hogi: TypeError: Cannot destructure property 'userId' of 'decodedToken' as it is undefined.
+    const { userId, role } = decodedToken; 
+
+    console.log("User verified:", userId);
 
   } catch (error) {
-    console.log("Crash detected! Sending to Live TraceLens Cloud...");
+    console.log("Crash detected! Sending to Render TraceLens Cloud...");
 
-    // Yahan tumhara LIVE Back4App ka URL hai
-    await fetch('https://tracelens-0dakofo6.b4a.run/api/incidents', {
+    // UPDATE: Yahan naya Render ka API URL daala hai
+    await fetch('https://tracelens-7hdm.onrender.com/api/incidents', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -23,7 +25,7 @@ async function triggerLiveError() {
       body: JSON.stringify({
         message: error.message,
         stackTrace: error.stack,
-        service: 'production-auth-service' // Nayi service ka naam
+        service: 'production-auth-service'
       })
     });
     
